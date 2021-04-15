1c369d7b9fabd3413a49296f0f0be693f460969c
Simon K. Mortensen <zimonqm@hotmail.com>
2021-04-15 16:00:42 +0200

    [Added] Repository interface.

--- src/main/java/dk/howard/repository/entity/AnsweredQuestionPO.java
+++ src/main/java/dk/howard/repository/entity/AnsweredQuestionPO.java
@@ -1,2 +1,71 @@
-package dk.howard.repository.entity;public class AnsweredQuestionPO {
+package dk.howard.repository.entity;
+
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "AnsweredQuestion")
+public class AnsweredQuestionPO {
+
+    public static final String FIND_ALL = "AnsweredQuestionPO.findAll";
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(name = "ID", columnDefinition = "VARCHAR(40)", nullable = false, updatable = false, unique = true)
+    private String id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "Question", referencedColumnName = "ID")
+    private QuestionPO questionPO;
+
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    private AnswerPO answerPO;
+
+    public AnsweredQuestionPO(){
+        this.questionPO = null;
+        this.answerPO = null;
+    }
+
+    public AnsweredQuestionPO(QuestionPO question, AnswerPO answer){
+        this.questionPO = question;
+        this.answerPO = answer;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public QuestionPO getQuestionPO() {
+        return questionPO;
+    }
+
+    public void setQuestionPO(QuestionPO questionPO) {
+        this.questionPO = questionPO;
+    }
+
+    public AnswerPO getAnswerPO() {
+        return answerPO;
+    }
+
+    public void setAnswerPO(AnswerPO answerPO) {
+        this.answerPO = answerPO;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("ID: " + this.id);
+        sb.append(", Question: " + this.questionPO);
+        sb.append(", Answer: " + this.answerPO);
+        return sb.toString();
+    }
 }
--- src/main/java/dk/howard/repository/interfaces/IRepository.java
+++ src/main/java/dk/howard/repository/interfaces/IRepository.java
@@ -1,2 +1,11 @@
-package dk.howard.repository.interfaces;public interface IRepository {
+package dk.howard.repository.interfaces;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public interface IRepository<T> {
+    public T remove(T entity);
+    public Set<T> findAll();
+    public void add(T entity);
+    public T getById(String id);
 }
