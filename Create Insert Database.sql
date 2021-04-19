f676c366dc4223dddd91cb66620bdfc500ee5841
saphyron <30288325+saphyron@users.noreply.github.com>
2021-04-19 11:16:19 +0200

    SQL update + Server con
    forbindelse

--- Create Insert Database.sql
+++ Create Insert Database.sql
@@ -1,5 +1,5 @@
-drop table if exists questions
 drop table if exists answers
+drop table if exists questions
 
 create table questions(
 id int identity primary key,
@@ -10,20 +10,20 @@ question text not null,
 )
 
 create table answers(
-id int primary key,
+id int identity primary key,
 AnswerName varchar(40),
 questions int foreign key references questions(id),
 trueAnswer bit,
 forklaring text,
-link varchar(40)
+link varchar(150)
 )
 
 insert into questions values
 ('SCIENCE','Physics',12,'Wich physical property can be measured in the unit Coulomb')
 
 insert into answers values
-('Pressure', 1, 0, 'Not Correct', 'Link to correct answer: www.link.dk')
-('Temperature', 1, 0, 'Not Correct', 'Link to correct answer: www.link.dk')
-('Volume', 1, 0, 'Not Correct', 'Link to correct answer: www.link.dk')
-('Force', 1, 0, 'Not Correct', 'Link to correct answer: www.link.dk')
+('Pressure', 1, 0, 'Not Correct', 'Link to correct answer: www.link.dk'),
+('Temperature', 1, 0, 'Not Correct', 'Link to correct answer: www.link.dk'),
+('Volume', 1, 0, 'Not Correct', 'Link to correct answer: www.link.dk'),
+('Force', 1, 0, 'Not Correct', 'Link to correct answer: www.link.dk'),
 ('Charge', 1, 1, 'he SI unit for electric charge is Coulomb', 'https://en.wikipedia.org/wiki/Pascal_(unit)')
